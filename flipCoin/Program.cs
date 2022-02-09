ecbfc90b8931ec0694883aaebfe356a7295b2cf5
rounak <rounakkole@gmail.com>
2022-02-09 16:44:38 +0530

    Add project files.

---
+++ flipCoin/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace flipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("flip coin simulator");
+        }
+    }
+}
